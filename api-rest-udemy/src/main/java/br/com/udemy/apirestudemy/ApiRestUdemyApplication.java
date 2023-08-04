397987dd74b7c3ec6f7240daca1e2bfabaeed3ab
perigorvladimir <120771347+perigorvladimir@users.noreply.github.com>
2023-08-04 17:39:00 -0300

    Add files via upload

---
+++ api-rest-udemy/src/main/java/br/com/udemy/apirestudemy/ApiRestUdemyApplication.java
@@ -0,0 +1,12 @@
+package br.com.udemy.apirestudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestUdemyApplication.class, args);
+    }
+}
---
+++ api-rest-udemy/src/test/java/br/com/udemy/apirestudemy/ApiRestUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.apirestudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
